a444b84d035a0ffa2188e8c8b52f1fd7775c2eca
Facundo Torres <facutor@gmail.com>
2023-11-28 14:42:42 -0300

    change

--- Proyecto-Torres/back/app.js
+++ Proyecto-Torres/back/app.js
@@ -8,10 +8,8 @@ require('dotenv').config();
 
 var pool = require('./models/bd');
 
-
 var loginRouter = require('./routes/admin/login');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 var app = express();
 
@@ -24,14 +22,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin/login', loginRouter);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('./admin/login', loginRouter);
+
 //armar distintas consultas
-// pool.query('select * from usuarios').then(function(resultados){
+ //pool.query('select * from usuarios').then(function(resultados){
 //   console.log(resultados)
-// });
+//});
 
  
 
