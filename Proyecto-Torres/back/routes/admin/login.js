54f103dea8b4d6fab3762075ad189a70eba5fa5e
Facundo Torres <facutor@gmail.com>
2023-11-29 11:51:48 -0300

    changes

--- Proyecto-Torres/back/routes/admin/login.js
+++ Proyecto-Torres/back/routes/admin/login.js
@@ -17,10 +17,13 @@ router.post('/', async (req, res, next) => {
     var password = req.body.password;
 
     var data = await usuariosModel.getUserByUsernameAndPassword(usuario, password);
+
     if (data != undefined) {
+      req.session.id_usuario = data.id;
+      req.session.nombre = data.usuario;
       res.redirect('/admin/novedades');
     } else {
-      res.render('admin/login',{
+      res.render('admin/login', {
         layout: 'admin/layout',
         error: true
       });
