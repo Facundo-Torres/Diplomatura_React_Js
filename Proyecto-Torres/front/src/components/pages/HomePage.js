d3ec6aac9d377872c51e1d3354200d1b2dfdbe98
Facundo Torres <facutor@gmail.com>
2023-11-30 21:27:05 -0300

    change

--- Proyecto-Torres/front/src/components/pages/HomePage.js
+++ Proyecto-Torres/front/src/components/pages/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, Row, Col, Card, Form, Button, FloatingLabel, ListGroup } from 'react-bootstrap';
+import { Container, Row, Col, Card, ListGroup } from 'react-bootstrap';
 
 
 
